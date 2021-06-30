28921b19067da9023743bf063d7ba16430916bdb
The Hoang <thehoangcn511@gmail.com>
2021-06-30 22:37:57 +0700

    Liệt kê file truy vấn vào index.txt + Trích lọc từ đơn vào metadata

--- Search_Engine_Backend/test.cpp
+++ Search_Engine_Backend/test.cpp
@@ -6,12 +6,10 @@
 #include <iostream>
 #include <fstream>
 #include <windows.h>
-#include <iterator> // Su dung tam thoi --> sau do thay bang hash
-#include <map> // Su dung tam thoi --> sau do thay bang hash
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-map<wstring, int> cnt;
-map<wstring, bool> isStopword;
 
 void removeStopWord(const wchar_t* name){
     HANDLE cur;
@@ -19,41 +17,60 @@ void removeStopWord(const wchar_t* name){
     wchar_t meta[50], index[50];
     wcscpy(meta, name); wcscat(meta, L"\\metadata.dat");
     wcscpy(index, name); wcscat(index, L"\\index.text");
-    FILE* fileMeta = _wfopen(meta, L"a");
+    FILE* fileMeta = _wfopen(meta, L"w,ccs=UTF-8");
     FILE* fileIndex = _wfopen(index, L"w");
 
     wstring newName(name);
     newName += L"\\*.txt";
     wprintf(L"%s\n", index);
     if ((cur = FindFirstFileW(newName.c_str(), &wfd)) != INVALID_HANDLE_VALUE){
+        int cnt = 0;
         do{
             fwprintf(fileIndex, L"%s\n", wfd.cFileName);
             wstring curNamePath(name);
             curNamePath += L"\\";
             curNamePath += wfd.cFileName;
-            FILE* curPath = _wfopen(curNamePath.c_str(), L"r");
-            wprintf(L"%s\n", curNamePath.c_str());
+            FILE* curPath = _wfopen(curNamePath.c_str(), L"r,ccs=UTF-8");
+            wstring s = L"";
             wchar_t buffer[40];
-            /*
-            while (fwscanf(curPath, L"%s", buffer)){
-                int n = wcslen(buffer);
-                if (buffer[n - 1] == L'\n'){
-                    buffer[n - 1] = L'\0';
-                }
+            vector<wstring> memo;
+            while (fwscanf(curPath, L"%s", buffer)){  
                 wstring s(buffer);
-                if (isStopword[s]) continue;
-                else {
-                    cnt[s]++;
+                memo.push_back(s);
+            }
+            sort(memo.begin(), memo.end());
+            wstring word = memo[0];
+            cnt = 1;
+            fwprintf(fileMeta, L"%s\n", curNamePath.c_str());
+            for (int i = 1; i < memo.size(); i++){
+                if (memo[i] == memo[i - 1]){
+                    cnt++;
+                }
+                else{
+                    fwprintf(fileMeta, L"%s %d\n", word.c_str(), cnt);
+                    cnt = 1;
+                    word = memo[i];
                 }
             }
+            fwprintf(fileMeta, L"%s %d\n", word.c_str(), cnt);
+            /*
+            while (true){
+                if (feof(curPath)){
+                    break;
+                }
+                s += fgetwc(curPath);
+            }
+            s.erase(s.size() - 1, 1);
+            fwprintf(fileMeta, L"%s\n", s.c_str());
             */
-            fwprintf(fileMeta, L"%s\n", curNamePath.c_str());
+            fclose(curPath);
         } while (FindNextFileW(cur, &wfd));
         FindClose(cur);
     }
 
 }
 
+/*
 void prepare(){
     FILE* f = _wfopen(L"stopwords.txt", L"r");
     wchar_t buffer[40];
@@ -64,11 +81,11 @@ void prepare(){
         isStopword[buffer] = true;
     }
 }
+*/
 
 int main(int argc, char* argv[])
 {
-    //_setmode(_fileno(stdout), _O_U16TEXT); 
-    //_setmode(_fileno(stdin), _O_U16TEXT);
-    prepare();
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    //prepare();
     removeStopWord(L"train\\new train\\Am nhac");
 }
